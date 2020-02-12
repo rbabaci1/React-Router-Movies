f916e13a9fe431550230c575d0814c6ab0ea0a77
rabah babaci <4rabah@gmail.com>
2020-02-12 15:46:24 -0800

    add a route to render movie liste component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Route } from "react-router-dom";
 
 import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -14,6 +15,7 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+      <Route path="/" component={MovieList} />
     </div>
   );
 };
