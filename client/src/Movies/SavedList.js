537d4480039d89bf5738592749cfd10d479d6369
rabah babaci <4rabah@gmail.com>
2020-02-12 17:24:40 -0800

    turn the list of saved movies into links to its info

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const SavedList = props => (
+const SavedList = ({ list }) => (
   <div className="saved-list">
-    {console.log(props)}
     <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+    {list.map(movie => (
+      <Link to={`/movies/${movie.id}`}>
+        <span className="saved-movie">{movie.title}</span>
+      </Link>
     ))}
     <Link to="/">
       <div className="home-button">Home</div>
