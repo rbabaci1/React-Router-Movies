a4b9d321886f27c9b9ab4bcf37f7c80818537ed4
rabah babaci <4rabah@gmail.com>
2020-02-12 17:08:06 -0800

    uncomment the saveMovie function

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -17,11 +17,10 @@ const Movie = props => {
       });
   }, [id]);
 
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie);
+  };
 
   if (!movie) {
     return <div>Loading movie information...</div>;
