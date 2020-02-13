5d54f65a5ca745ea750f88c2fe93e5fd1d70e05d
rabah babaci <4rabah@gmail.com>
2020-02-12 19:46:40 -0800

    turn saved movies into NavLink with style

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+
+const navStyle = {
+  fontWeight: "bold",
+  background: "#f5e5d0",
+  padding: "4px 0",
+  fontFamily: "Monaco",
+  color: "#0d9980",
+  margin: "0 15px 10px 0"
+};
 
 const SavedList = ({ list }) => {
   return (
@@ -7,12 +16,12 @@ const SavedList = ({ list }) => {
       <h3>Saved Movies:</h3>
       {list.map(movie => {
         return (
-          <Link to={`/movies/${movie.id}`} key={movie.id}>
+          <NavLink style={navStyle} to={`/movies/${movie.id}`} key={movie.id}>
             <span className="saved-movie">{movie.title}</span>
-          </Link>
+          </NavLink>
         );
       })}
-      <Link to="/">
+      <Link to="/" className="home-link">
         <div className="home-button">Home</div>
       </Link>
     </div>
