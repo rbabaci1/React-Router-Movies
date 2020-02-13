56114c81b6039b9ef656f8c174cf45105fa9c1d4
rabah babaci <4rabah@gmail.com>
2020-02-12 16:33:40 -0800

    DRY the code in Movie component

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -3,26 +3,27 @@ import React from "react";
 const MovieCard = props => {
   const { title, director, metascore, stars } = props.movie;
 
-  return null;
-  // <div className="save-wrapper">
-  //   <div className="movie-card">
-  //     <h2>{title}</h2>
-  //     <div className="movie-director">
-  //       Director: <em>{director}</em>
-  //     </div>
-  //     <div className="movie-metascore">
-  //       Metascore: <strong>{metascore}</strong>
-  //     </div>
-  //     <h3>Actors</h3>
+  return (
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
 
-  //     {stars.map(star => (
-  //       <div key={star} className="movie-star">
-  //         {star}
-  //       </div>
-  //     ))}
-  //   </div>
-  //   <div className="save-button">Save</div>
-  // </div>
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button">Save</div>
+    </div>
+  );
 };
 
 export default MovieCard;
