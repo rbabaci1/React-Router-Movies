122d076d7ae3aba2f0564b601ffa92d7b5774aa1
rabah babaci <4rabah@gmail.com>
2020-02-12 16:01:35 -0800

    make every movie card a link to its info

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -22,7 +22,9 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
